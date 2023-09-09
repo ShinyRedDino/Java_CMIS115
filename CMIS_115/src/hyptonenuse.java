9b1e0802935a03c595797c5c3d1757b9c1c49231
ShinyRedDino <jeremyhcim1993@gmail.com>
2023-09-09 13:59:21 +0200

    Max/Hyptoneuse

---
+++ CMIS_115/src/hyptonenuse.java
@@ -0,0 +1,13 @@
+
+public class hyptonenuse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a = 5.0;
+		double b = 2.0;
+		double c = Math.sqrt(a * a + b * b);
+		
+		System.out.print(c);
+	}
+
+}
---
+++ CMIS_115/src/max.java
@@ -0,0 +1,12 @@
+
+public class max {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int population1 = 12000;
+		int population2 = 11592;
+		
+		System.out.print(Math.max(population1, population2));
+	}
+
+}
